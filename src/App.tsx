fa18b32b38e27409a0db9485bfbb5010cff66028
gunturcab1-oss <gunturcab1@gmail.com>
2026-08-25 22:48:57 +0530

    Route driver login and home preview

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from '@/pages/Home.tsx';
 import Booking from '@/pages/Booking.tsx';
 import DriverRequest from '@/pages/DriverRequest.tsx';
 import AdminDrivers from '@/pages/AdminDrivers.tsx';
+import DriverLogin from '@/pages/DriverLogin.tsx';
+import DriverHome from '@/pages/DriverHome.tsx';
 import SEOPage from '@/components/SEOPage.tsx';
 
 const App: React.FC = () => {
@@ -13,6 +15,8 @@ const App: React.FC = () => {
       <Route path="/book" element={<Booking />} />
       <Route path="/driver-preview" element={<DriverRequest />} />
       <Route path="/admin-drivers" element={<AdminDrivers />} />
+      <Route path="/driver-login" element={<DriverLogin />} />
+      <Route path="/driver-home" element={<DriverHome />} />
       <Route path="/:url" element={<SEOPage />} />
     </Routes>
   );
