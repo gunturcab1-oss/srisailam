59e6473a5a41682db144c7e9c91e6d5148a2609c
gunturcab1-oss <gunturcab1@gmail.com>
2026-08-25 19:15:36 +0530

    Route booking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home.tsx';
+import Booking from '@/pages/Booking.tsx';
 import SEOPage from '@/components/SEOPage.tsx';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/book" element={<Booking />} />
       <Route path="/:url" element={<SEOPage />} />
     </Routes>
   );
