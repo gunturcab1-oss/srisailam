cb7547c61435c35b9a232780884d041e32a8b456
gunturcab1-oss <gunturcab1@gmail.com>
2026-08-25 21:49:13 +0530

    Add admin driver management route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home.tsx';
 import Booking from '@/pages/Booking.tsx';
 import DriverRequest from '@/pages/DriverRequest.tsx';
+import AdminDrivers from '@/pages/AdminDrivers.tsx';
 import SEOPage from '@/components/SEOPage.tsx';
 
 const App: React.FC = () => {
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/book" element={<Booking />} />
       <Route path="/driver-preview" element={<DriverRequest />} />
+      <Route path="/admin-drivers" element={<AdminDrivers />} />
       <Route path="/:url" element={<SEOPage />} />
     </Routes>
   );
