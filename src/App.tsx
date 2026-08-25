ae709e685ba0db19bda55108cf5ade1386232a0c
gunturcab1-oss <gunturcab1@gmail.com>
2026-08-25 22:57:34 +0530

    Add admin booking assignment route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from '@/pages/Home.tsx';
 import Booking from '@/pages/Booking.tsx';
 import DriverRequest from '@/pages/DriverRequest.tsx';
 import AdminDrivers from '@/pages/AdminDrivers.tsx';
+import AdminBookings from '@/pages/AdminBookings.tsx';
 import DriverLogin from '@/pages/DriverLogin.tsx';
 import DriverHome from '@/pages/DriverHome.tsx';
 import SEOPage from '@/components/SEOPage.tsx';
@@ -15,6 +16,7 @@ const App: React.FC = () => {
       <Route path="/book" element={<Booking />} />
       <Route path="/driver-preview" element={<DriverRequest />} />
       <Route path="/admin-drivers" element={<AdminDrivers />} />
+      <Route path="/admin-bookings" element={<AdminBookings />} />
       <Route path="/driver-login" element={<DriverLogin />} />
       <Route path="/driver-home" element={<DriverHome />} />
       <Route path="/:url" element={<SEOPage />} />
